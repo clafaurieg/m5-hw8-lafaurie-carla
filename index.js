b495880824d9e16a6b1d4c1a70c52d1097273e3f
clafaurieg <70864691+clafaurieg@users.noreply.github.com>
2022-03-24 23:17:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-
 // Create an Array of at least 3 losing messages
+var messages = [
+    "Try Again",
+    "Incorrect",
+    "Wrong"
+]
 
-let losingMessage = ["Bummer, you lost!", "You Lose.", "Try again, because you lost!"];
 
 // Create variables to count wins and losses
 var wins = 0
