81b80c2de4f81e1e10ad6035ccf4b2600c5e4360
clafaurieg <70864691+clafaurieg@users.noreply.github.com>
2022-03-24 22:36:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
 // Create an Array of at least 3 losing messages
-let messages = [
-    "You Lost",
-    "Sorry, Try Again!",
+var messages = [
+    "Try Again",
+    "Incorrect",
     "Wrong"
 ]
 
 // Create variables to count wins and losses
+var wins = 0
+var losses = 0
 
-let wins = 0
-let losses = 0
 
 // Create variables that target elements with the following IDs: 'message', 'wins', 'losses'
-
-let message = document.getElementById("message")
-let wins = document.getElementById("wins")
-let losses = document.getElementById("losses")
-
+var messageE1 = document.getElementById("message")
+var winsE1 = document.getElementById("wins")
+var lossesE1 = document.getElementById("losses")
 // target all .box elements and attach a click event listener to each one using a loop
-
 var boxElements = document.querySelectorAll(".box")
 
 for (var i = 0; i < boxElements.length; i++) {
     var boxElement = boxElements[i];
-    
-    
-// within each click event...
-// determine which box was clicked with 'this.textContent' or event.target.textContent
-// convert that value to a Number and store it to a variable
 
-boxElement.onclick = function (event) {
-            
-// create a random number between 1-3 and store it to a variable
-// This number will represent the winning box
-    
-var boxNumberWasClicked = Number(event.target.textContent)
+    // within each click event...
+    // determine which box was clicked with 'this.textContent' or event.target.textContent
+    // convert that value to a Number and store it to a variable
+
+    boxElement.onclick = function (event) {
+
+        // create a random number between 1-3 and store it to a variable
+        // This number will represent the winning box
+        var boxNumWasClicked = Number(event.target.textContent)
         var random = Math.ceil(Math.random() * 3);
 
-        if (boxNumberWasClicked === random) {
-            message.textContent = "Accurate!"
+        if (boxNumWasClicked === random) {
+            messageE1.textContent = "Correct!"
             wins++
-            wins.textContent = "Wins: " + wins
+            winsE1.textContent = "Wins: " + wins
         }
         else {
             var random = Math.floor(Math.random() * 3);
-            message.textContent = messages[random];
+            messageE1.textContent = messages[random];
             losses++
-            losses.textContent = "Losses: " + losses
+            lossesE1.textContent = "Losses: " + losses
         }
 
     }
-// determine if the box clicked is equal to the random number
-// if the numbers match, display a winning message by changing the text content of the div#message element
-// if the numbers match, increment wins and display the win count in div#wins
 
+
+
+    // determine if the box clicked is equal to the random number
+    // if the numbers match, display a winning message by changing the text content of the div#message element
+    // if the numbers match, increment wins and display the win count in div#wins
+
+
+}
 // if the numbers don't match, change the div#message element's text to a random losing message from the array above
 // if the numbers don't match, increment losses and display the loss count in div#losses
